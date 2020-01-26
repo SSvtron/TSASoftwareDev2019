464b76df92de55721b261b5bf4a11881cbb9ac60
Shreshth Kharbanda <sk04us@gmail.com>
2020-01-26 03:20:39 -0800

    Added Firebase
    Initial setup of Firebase.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,24 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// import the core Firebase SDK
+import * as firebase from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyAaivfEfCkBl5UR6bJqqlwhDhlceFzrxJA',
+	authDomain: 'studyzone-official.firebaseapp.com',
+	databaseURL: 'https://studyzone-official.firebaseio.com',
+	projectId: 'studyzone-official',
+	storageBucket: 'studyzone-official.appspot.com',
+	messagingSenderId: '214583740333',
+	appId: '1:214583740333:web:03111cc9f885a33de20324',
+	measurementId: 'G-FX04ERNDHY'
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 ReactDOM.render(
     <BrowserRouter>
         <App />
