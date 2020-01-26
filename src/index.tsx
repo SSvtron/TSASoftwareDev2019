53cd4ab20888da3cf1b1fbe548ad30c52cc31ada
shreshthkharbanda <jadook2003@gmail.com>
2020-01-26 02:50:13 -0800

    Revert "Added firebase"
    This reverts commit dd1ed1355867dfdf108fac9e8dac3f382c52b628.

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
-);
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, document.getElementById('root'));
